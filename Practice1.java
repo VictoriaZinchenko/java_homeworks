7f06c005eccd00f63fe93f8e885908bb55b03d5f
VictoriaZinchenko <47721481+VictoriaZinchenko@users.noreply.github.com>
2019-02-18 16:09:30 +0200

    Add files via upload

---
+++ Practice1.java
@@ -0,0 +1,13 @@
+package com.groupqa1;
+
+public class Practice1 {
+    public static void main(String[] args) {
+        System.out.println("****  ****      *      *     *");
+        System.out.println("*     *  *     * *     * * * *");
+        System.out.println("**    ****    *   *    *  *  *") ;
+        System.out.println("*     *      *******   *     *");
+        System.out.println("****  *     *       *  *     *");
+
+    }
+}
+
