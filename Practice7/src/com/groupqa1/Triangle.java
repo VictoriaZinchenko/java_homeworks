9eb92672dc5c88a6d09172d089b53094f2777de9
VictoriaZinchenko <47721481+VictoriaZinchenko@users.noreply.github.com>
2019-04-07 17:42:57 +0300

    Update Triangle.java

--- Practice7/src/com/groupqa1/Triangle.java
+++ Practice7/src/com/groupqa1/Triangle.java
@@ -5,12 +5,13 @@ public class Triangle extends Shape{
     double b;
     double c;
     final int two = 2;
-    String color = "green";
+
 
     public Triangle() {
     }
 
-    public Triangle(double a, double b, double c) {
+    public Triangle(String color, double a, double b, double c) {
+        super(color);
         this.a = a;
         this.b = b;
         this.c = c;
@@ -18,14 +19,14 @@ public class Triangle extends Shape{
     @Override
     public String toString()
     {
-        return "class = Triangle;" + " color = " + color
-                + "\n The sides of the triangle a = " + a + "; b = " + b + "; c = " + c;
+        return super.toString() + ". The sides of the triangle: a = " + a + "; b = " + b + "; c = " + c;
     }
     @Override
     public double calcArea(){
         double  p = (a + b + c)/two;
-        area = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        double area = Math.sqrt(p * (p - a) * (p - b) * (p - c));
         return area;
+
     }
 
 
